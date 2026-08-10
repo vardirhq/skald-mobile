a4eac6a1d44d466671cf880e3658f485997c8437
Christoffer Madsen <126353638+MadsenDev@users.noreply.github.com>
2026-08-10 22:07:08 +0200

    Use separate dev application ID for debug builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
         debug {
-            applicationIdSuffix = ".debug"
+            applicationIdSuffix = ".dev"
         }
     }
 
